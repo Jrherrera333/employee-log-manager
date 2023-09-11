08dd24115a1aaa62c611344812109dced37c5283
Jose Herrera <herrera.joserafael@gmail.com>
2023-09-11 01:24:54 -0400

    fix inquirer error

--- mysqlConnector.js
+++ mysqlConnector.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
   host: 'localhost',         // Your MySQL server's hostname
   user: 'root',      // Your MySQL username
-  password: 'rafy07',
+  password: '',
   database: 'employee_log_manager'   // Your MySQL database name
 });
 
